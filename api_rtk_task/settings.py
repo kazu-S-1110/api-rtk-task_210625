4393d145a011e564044a82fd5fb8ff26643b3f08
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-29 12:39:12 +0900

    settings.pyを編集。databaseのstrを追記してdeployができるか。

--- api_rtk_task/settings.py
+++ api_rtk_task/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'api_rtk_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
